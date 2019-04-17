d169f9a02ef3e612c0acd0473da99da5dd7ea3fe
soyoung <33671550+soyoungkwon@users.noreply.github.com>
2019-04-17 15:25:46 +0200

    anova : pandas series made, with error

--- 00_BatchFile/main_linear_stats.py
+++ 00_BatchFile/main_linear_stats.py
@@ -39,39 +39,32 @@ for dxn in range(8):
 
 
 # change numpy --> pandas dataframe
-gain_rest_pd = pd.DataFrame(gain_rest.T, index = dxn_name).T
-gain_rest.shape
-len(dxn_name)
-sub_id = np.matlib.repmat(np.arange(20), 1, 8)
-sub_id.shape
-a = np.matlib.repmat('FEF', 2, 1), np.matlib.repmat('IPS', 2, 1)
+# gain_rest_pd = pd.DataFrame(gain_rest.T, index = dxn_name).T
+
+sub_id_all = np.matlib.repmat(np.arange(20), 1, 8).T
+sub_id_1d = sub_id_all.tolist()
+sub_id_1d
+# a = np.matlib.repmat('FEF', 2, 1), np.matlib.repmat('IPS', 2, 1)
 area = []
 for rep1 in range(20):
     for d_name in DXN_Name:
         for rep2 in range(2):
             area  += [d_name]
-area.shape
+hemi = []
+for rep3  in range(80):
+    for n in ['L', 'R']:
+        hemi += n
+len(hemi)
+# create coef list
+gain_rest_1d = gain_rest.T.ravel().tolist()
 
-gain_rest_1d = gain_rest.T.ravel()
-gain_rest_1d = gain_rest_1d[..., np.newaxis]
+# creat sub_id list
+sub_id_once = (np.arange(20).reshape(1,20))
+sub_id_all_np = np.repeat(sub_id_once, 8,0).ravel()
+sub_id_all_list = sub_id_all_np.tolist()
 
-a=np.concatenate((sub_id.T, gain_rest_1d), axis=1)
-b=a.tolist()
-len(b)
-c=area + b
-len(c)
-pd.DataFrame(a)
-df = pd.DataFrame(a.T, index = ['sub_id', 'coef']).T# 'coef': gain_rest_1d})
-
-# numpy to list
-sub_id_list = sub_id.tolist()
-gain_rest_list = gain_rest_1d.tolist()#tuple(map(tuple, gain_rest_1d.T))
-
-A1 = ('sub_id', sub_id_list)
-A2 = ('gain_rest', gain_rest_list)
-A = [A1, A2]
-sub_id_tuple[0]
-A = [sub_id_list, gain_rest_list]
-t = pd.DataFrame.from_items(A1)
-type(A1)
-A1[1]
+# create pandas
+help(AnovaRM)
+gain_rest_pd = pd.DataFrame({'sub_id': sub_id_all_list, 'gain_rest': gain_rest_1d, 'area': area, 'hemi': hemi})
+aovrm2way = AnovaRM(gain_rest_pd, 'gain_rest', 'sub_id', within = ['area', 'hemi'])#, aggregate_func = 'mean')
+print(gain_rest_pd)
